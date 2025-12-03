a81fdd62a1492cd85f33f4011ee3121c744f42db
nitin <pantnitin29@gmail.com>
2025-12-03 14:42:23 +0530

    first push

---
+++ project1/src/test/java/pack_1/contactTest.java
@@ -0,0 +1,27 @@
+package pack_1;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+	
+	@Test
+	
+	public void createContactTest() {
+		String URL=System.getProperty("browser","chrome");
+		String BROWSER = System.getProperty("username");
+		String USERNAME= System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("Execute Create contact test");
+	}
+@Test
+public void modifyContactTest() {
+	System.out.println("Modify contact test");
+	
+	
+}
+}
---
+++ project1/src/test/java/pack_2/createOrgTest.java
@@ -0,0 +1,23 @@
+package pack_2;
+
+import org.testng.annotations.Test;
+
+public class createOrgTest {
+
+	@Test
+	
+	public void createOrggTest() {
+		System.out.println("Create org test");
+	}
+	
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("Modify Org test");
+	
+	
+	
+	}
+	
+	
+}
